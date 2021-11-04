b6da515be46d91cde896ee86ab288a7f4e3ab2ed
lim ho <dlagh123@gmail.com>
2021-11-04 15:51:37 +0900

    .

--- 5-generic/5-1-function.ts
+++ 5-generic/5-1-function.ts
@@ -1,5 +1,22 @@
 {
-    function checkNotNull(arg: number): number {
-
+    function checkNotNullBad(arg: number | null): number {
+        if (arg == null) {
+            throw new Error("not valid number!");
+        }
+        return arg;
     }
+    const result = checkNotNullBad(123);
+    console.log(result);
+    checkNotNullBad(null);
+
+
+
+    function checkNotNull<T>(arg: T | null): T {
+        if (arg == null) {
+            throw new Error("not valid number!");
+        }
+        return arg;
+    }
+    const number: number = checkNotNull(123);
+    const boal: boolean = checkNotNull(true);
 }
